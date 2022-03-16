0975fee84890064ecf6197445657723ae8863732
Turi <40395348+turinaf@users.noreply.github.com>
2022-03-16 11:33:25 +0800

    models & db migrations

--- base/views.py
+++ base/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 # from django.http import HttpResponse
 # We get rid of HttResponse after adding our template in Settings and use render
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': "Let learn Python"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Back end developers"},
-]
+# rooms = [
+#     {'id': 1, 'name': "Let learn Python"},
+#     {'id': 2, 'name': "Design with me"},
+#     {'id': 3, 'name': "Back end developers"},
+# ]
 
 def home(request):
-    return render(request, 'base/home.html', {'rooms':rooms})
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] ==  int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    # get used to get one single item from Room
     context = {'room': room}
     return render(request, 'base/room.html', context)
