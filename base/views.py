fc5b0df1e1e7354ccd994ecf06942dce9d41fad4
Turi <40395348+turinaf@users.noreply.github.com>
2022-03-16 22:17:03 +0800

    update room

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Room
 # from django.http import HttpResponse
@@ -27,6 +28,18 @@ def createRoom(request):
         # print(request.POST)
         form = RoomForm(request.POST)
         # is the form is valid, form data into db and redirect the user to home page.
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
