4558bf6c16b5da13ec8708726c1b6c4fd8c5cbd0
Turi <40395348+turinaf@users.noreply.github.com>
2022-03-17 10:46:30 +0800

    user login & logout

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
-import imp
-from multiprocessing import context
-from os import name
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # from django.http import HttpResponse
 # We get rid of HttResponse after adding our template in Settings and use render
 from .forms import RoomForm
@@ -14,6 +14,28 @@ from .forms import RoomForm
 #     {'id': 3, 'name': "Back end developers"},
 # ]
 
+def loginPage(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        user_password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=user_name)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user =  authenticate(request, username=user_name, password=user_password) 
+        if user is not None:
+            login(request, user) 
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist.')
+    context= {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
