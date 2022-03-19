4e6b399fce444aec5c6930c42ad9b5cc34e7348e
Turi <40395348+turinaf@users.noreply.github.com>
2022-03-19 09:40:47 +0800

    Added participants to the room

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -74,7 +74,19 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     # get used to get one single item from Room
-    context = {'room': room}
+    
+    # Let's get messages
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        new_massage = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 # Restricting unauthorized user from accessing some pages.
