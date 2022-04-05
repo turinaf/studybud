0a864349719d1a5ae9c7a7632ec4a636a141f545
Turi <40395348+turinaf@users.noreply.github.com>
2022-04-05 09:28:28 +0800

    edit user profile

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,8 @@
+from ast import Mod
 import imp
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -8,3 +10,8 @@ class RoomForm(ModelForm):
         fields = '__all__'
         exclude = ['host', 'participants']
         # exclude the two fields from form
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
